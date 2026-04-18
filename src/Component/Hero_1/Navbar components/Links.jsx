01628ef296084bf04b63b25573f9ab65add38fe4
abhidas0101001-beep <abhidas0101001@gmail.com>
2026-04-18 12:32:27 +0530

    Finished making the navbar

--- src/Component/Hero_1/Navbar components/Links.jsx
+++ src/Component/Hero_1/Navbar components/Links.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Links = () => {
-  return <div className="navBar_links">Links</div>;
+  return (
+    <div>
+      <ul className="navBar_links">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Project</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Links;
